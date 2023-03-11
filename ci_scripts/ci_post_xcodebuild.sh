d1f635bf753fd96ac744b5f01209b917d6c174a8
Abdhilabs <rizaabdhi@gmail.com>
2023-03-11 14:02:56 +0700

    update command to make script executable

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -11,5 +11,7 @@ then
     echo "Uploading Firebase Debug Build"
     unzip firebase-tools-macos.zip
 
+    chmod +x ./firebase-tools-macos
+
     ./firebase-tools-macos appdistribution:distribute $CI_AD_HOC_SIGNED_APP_PATH/CoffeeAbdhilabs0123.ipa --app 1:56209729540:ios:9af5fb43985ac9ebaced51 --token "$FIREBASE_TOKEN" --groups "Dev"
 fi
