2bce68341660734ed691eca62ae9352849707616
Abdhilabs <rizaabdhi@gmail.com>
2023-03-11 13:52:43 +0700

    add config distribution to firebase

---
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  ci_post_xcodebuild.sh
+#  Coffee
+#
+#  Created by Abdhi P on 11/03/23.
+#  
+
+if [ "$CI_WORKFLOW" = "Firebase Debug Build" ];
+then
+    echo "Uploading Firebase Debug Build"
+    unzip firebase-tools-macos.zip
+
+    ./firebase-tools-macos appdistribution:distribute $CI_AD_HOC_SIGNED_APP_PATH/CoffeeAbdhilabs0123.ipa --app 1:56209729540:ios:9af5fb43985ac9ebaced51 --token "$FIREBASE_TOKEN" --groups "Dev"
+fi
